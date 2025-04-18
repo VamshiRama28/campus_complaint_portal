6d7679a9844316d4f2ef6ee8cca29bb4a0ff1c46
VamshiRama28 <vamshirama28@gmail.com>
2025-04-18 23:56:06 +0530

    Update MyComplaint.jsx

--- frontend/src/components/Student/MyComplaint.jsx
+++ frontend/src/components/Student/MyComplaint.jsx
@@ -194,7 +194,7 @@ const MyComplaints = () => {
   useEffect(() => {
     const fetchComplaints = async () => {
       try {
-        const res = await axios.get('http://localhost:5000/api/complaints', {
+        const res = await axios.get('https://campus-complaint-portal.onrender.com/api/complaints', {
           headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
         });
         setComplaints(res.data);
